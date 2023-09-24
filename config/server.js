32a4acab4287e1d203b61181dbd0cbe7cc7d686d
Sushant Shukla <1805352@kiit.ac.in>
2023-09-24 16:32:42 +0530

    Project Initialized

---
+++ config/server.js
@@ -0,0 +1,4 @@
+module.exports = {
+    APPLICATION_ID: process.env.APPLICATION_ID || 'P4D1ARXquPbFdUkZcFyCOAvhqPcFD_erMkBVOEbKBVI',
+    SECRET: process.env.SECRET || 'chLcl_i1sXcN6L8fYsvgUezav0G9ks8kfO-VWBYEt5k'
+}
